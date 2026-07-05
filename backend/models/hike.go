9718b94bd7af15b5c9bec46f5a05db2e41edadc4
Tyler Won <won.tyler1@gmail.com>
2026-07-04 17:45:31 -0700

    Model updates
    Document fields, add cover photo field to Hike model, add json tags to models

--- backend/models/hike.go
+++ backend/models/hike.go
@@ -2,18 +2,40 @@ package models
 
 import (
 	"gorm.io/datatypes"
-	"gorm.io/gorm"
 )
 
 type Hike struct {
-	gorm.Model
-	Trail        string
-	Date         datatypes.Date
-	Notes        *string
-	Rating       float32
-	Difficulty   float32
-	Distance     float32
-	Elevation    uint
-	AllTrailsUrl *string
-	Photos       []Photo
+	// Primary key
+	ID uint `json:"id"`
+
+	// The trail that was hiked.
+	Trail string `json:"trail"`
+
+	// The date of the hike.
+	Date datatypes.Date `json:"date"`
+
+	// Notes on the hike.
+	Notes *string `json:"notes"`
+
+	// How enjoyable the hike was out of 5. Half values allowed.
+	Rating float32 `json:"rating"`
+
+	// How difficult the hike was out of 10. Half values allowed.
+	Difficulty float32 `json:"difficulty"`
+
+	// The distance (km) hiked.
+	Distance float32 `json:"distance"`
+
+	// The elevation (m) gained on the hike.
+	ElevationGain uint `json:"elevationGain"`
+
+	// Link to the AllTrails page for the trail.
+	AllTrailsUrl *string `json:"allTrailsUrl"`
+
+	// Photos taken on the hike.
+	Photos []Photo `json:"photos"`
+
+	// Photo to use as the thumbnail for the hike.
+	CoverPhotoID *uint  `json:"coverPhotoId"`
+	CoverPhoto   *Photo `json:"coverPhoto"`
 }
--- backend/models/photo.go
+++ backend/models/photo.go
@@ -1,12 +1,15 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 type Photo struct {
-	gorm.Model
-	HikeID  uint
-	SrcUrl  string
-	Caption *string
+	// Primary key
+	ID uint `json:"id"`
+
+	// Link to where the photo can be accessed.
+	SrcUrl string `json:"srcUrl"`
+
+	// Caption for the photo.
+	Caption *string `json:"caption"`
+
+	// The Hike the photo was taken on.
+	HikeID uint `json:"hikeId"`
 }
