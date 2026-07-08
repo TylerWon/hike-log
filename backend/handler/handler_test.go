bb41845fe55d4c131da4c74af9e3b878c3b773d6
Tyler Won <won.tyler1@gmail.com>
2026-07-07 22:21:12 -0700

    Group tests into a test suite

--- backend/handler/handler_test.go
+++ backend/handler/handler_test.go
@@ -10,35 +10,44 @@ import (
 	"github.com/TylerWon/hike-log/backend/handler"
 	"github.com/TylerWon/hike-log/backend/models"
 	"github.com/TylerWon/hike-log/backend/testutils"
-	"github.com/stretchr/testify/assert"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
 	"gorm.io/datatypes"
+	"gorm.io/gorm"
 )
 
-func TestListHike_Empty(t *testing.T) {
-	db := testutils.SetupTestDB(t)
-	defer testutils.TeardownTestDB(t, db)
-	router := testutils.SetupTestRouter(handler.New(db))
+type HandlerTestSuite struct {
+	suite.Suite
+	router *gin.Engine
+	db     *gorm.DB
+}
 
+func (suite *HandlerTestSuite) SetupTest() {
+	suite.db = testutils.SetupTestDB(suite.T())
+	suite.router = testutils.SetupTestRouter(handler.New(suite.db))
+}
+
+func (suite *HandlerTestSuite) TearDownTest() {
+	testutils.TeardownTestDB(suite.T(), suite.db)
+}
+
+func (suite *HandlerTestSuite) TestListHike_Empty() {
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/hikes", nil)
 	rec := httptest.NewRecorder()
-	router.ServeHTTP(rec, req)
+	suite.router.ServeHTTP(rec, req)
 
-	assert.Equal(t, http.StatusOK, rec.Code)
+	suite.Equal(http.StatusOK, rec.Code)
 
 	var hikes []models.Hike
 	err := json.Unmarshal(rec.Body.Bytes(), &hikes)
 	if err != nil {
-		t.Fatal("Failed to decode response: ", err)
+		suite.T().Fatal("Failed to decode response: ", err)
 	}
 
-	assert.Len(t, hikes, 0)
+	suite.Len(hikes, 0)
 }
 
-func TestListHike_ReturnsHikes(t *testing.T) {
-	db := testutils.SetupTestDB(t)
-	defer testutils.TeardownTestDB(t, db)
-	router := testutils.SetupTestRouter(handler.New(db))
-
+func (suite *HandlerTestSuite) TestListHike_ReturnsHikes() {
 	var hikes []*models.Hike
 	hike1 := models.Hike{
 		TrailName:     "Trail 1",
@@ -71,34 +80,30 @@ func TestListHike_ReturnsHikes(t *testing.T) {
 	}
 	hikes = append(hikes, &hike2)
 
-	result := db.Create(&hikes)
+	result := suite.db.Create(&hikes)
 	if result.Error != nil {
-		t.Fatal("Failed to create hikes: ", result.Error)
+		suite.T().Fatal("Failed to create hikes: ", result.Error)
 	}
 
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/hikes", nil)
 	rec := httptest.NewRecorder()
-	router.ServeHTTP(rec, req)
+	suite.router.ServeHTTP(rec, req)
 
-	assert.Equal(t, http.StatusOK, rec.Code)
+	suite.Equal(http.StatusOK, rec.Code)
 
 	var response []models.Hike
 	err := json.Unmarshal(rec.Body.Bytes(), &response)
 	if err != nil {
-		t.Fatal("Failed to decode response: ", err)
+		suite.T().Fatal("Failed to decode response: ", err)
 	}
 
-	assert.Len(t, response, 2)
-	assert.Equal(t, hike1, response[0])
+	suite.Len(response, 2)
+	suite.Equal(hike1, response[0])
 	hike2.Photos = nil // Set Photos field to nil since it's excluded from the response by the List endpoint
-	assert.Equal(t, hike2, response[1])
+	suite.Equal(hike2, response[1])
 }
 
-func TestRetrieveHike_Success(t *testing.T) {
-	db := testutils.SetupTestDB(t)
-	defer testutils.TeardownTestDB(t, db)
-	router := testutils.SetupTestRouter(handler.New(db))
-
+func (suite *HandlerTestSuite) TestRetrieveHike_Success() {
 	photos := []models.Photo{
 		{SrcUrl: "https://example.com/photo-1.jpg"},
 		{SrcUrl: "https://example.com/photo-2.jpg", Caption: "The peak"},
@@ -117,35 +122,35 @@ func TestRetrieveHike_Success(t *testing.T) {
 		CoverPhoto:    &photos[0],
 	}
 
-	result := db.Create(&hike)
+	result := suite.db.Create(&hike)
 	if result.Error != nil {
-		t.Fatal("Failed to create hike: ", result.Error)
+		suite.T().Fatal("Failed to create hike: ", result.Error)
 	}
 
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/hikes/1", nil)
 	rec := httptest.NewRecorder()
-	router.ServeHTTP(rec, req)
+	suite.router.ServeHTTP(rec, req)
 
-	assert.Equal(t, http.StatusOK, rec.Code)
+	suite.Equal(http.StatusOK, rec.Code)
 
 	var response models.Hike
 	err := json.Unmarshal(rec.Body.Bytes(), &response)
 	if err != nil {
-		t.Fatal("Failed to decode response: ", err)
+		suite.T().Fatal("Failed to decode response: ", err)
 	}
 
 	hike.CoverPhoto = nil // Set CoverPhoto field to nil since it's excluded from the response by the Retrieve endpoint
-	assert.Equal(t, hike, response)
+	suite.Equal(hike, response)
 }
 
-func TestRetrieveHike_NotFound(t *testing.T) {
-	db := testutils.SetupTestDB(t)
-	defer testutils.TeardownTestDB(t, db)
-	router := testutils.SetupTestRouter(handler.New(db))
-
+func (suite *HandlerTestSuite) TestRetrieveHike_NotFound() {
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/hikes/999", nil)
 	rec := httptest.NewRecorder()
-	router.ServeHTTP(rec, req)
+	suite.router.ServeHTTP(rec, req)
 
-	assert.Equal(t, http.StatusNotFound, rec.Code)
+	suite.Equal(http.StatusNotFound, rec.Code)
+}
+
+func TestHandlerTestSuite(t *testing.T) {
+	suite.Run(t, new(HandlerTestSuite))
 }
