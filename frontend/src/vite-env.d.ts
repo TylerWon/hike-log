1b919863d1edda3e5c66dccee172d5d952db85a7
Tyler Won <won.tyler1@gmail.com>
2026-07-22 19:06:10 -0700

    Create .env files for frontend and backend development

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+// Add environment variables here for TypeScript IntelliSense to work
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
