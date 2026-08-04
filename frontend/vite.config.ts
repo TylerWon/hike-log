66bab6342b48d6071ede4cb5eea2ebad70b7b70a
Tyler Won <won.tyler1@gmail.com>
2026-08-03 22:10:57 -0700

    Configure Vitest and Vitest Browser Mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,28 @@
+/// <reference types="vitest/config" />
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
-  // Settings for Docker Compose compatilibity
   server: {
+    // Settings for dev server to work with Docker Compose
     host: true,
     port: 5173,
     watch: {
       usePolling: true,
+    },
+  },
+  test: {
+    // Vitest Browser Mode settings
+    browser: {
+      enabled: true,
+      headless: true,
+      instances: [{ browser: "chromium" }],
+      provider: playwright(),
     },
   },
 });
