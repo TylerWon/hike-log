7420116ce91d9523025f31210ec4414098d75d6c
Tyler Won <won.tyler1@gmail.com>
2026-08-16 20:27:23 -0700

    Add toSerializableJsonObj tests

---
+++ frontend/src/tests/utils/json.test.ts
@@ -0,0 +1,54 @@
+import { describe, expect, test } from "vitest";
+
+import { toSerializableJsonObj } from "../../utils/json";
+
+describe("json", () => {
+  describe("toSerializableJsonObj", () => {
+    test("returns a copy of original object when it is already serializable", async () => {
+      const input = {
+        category: "Sports Equipment",
+        id: 1,
+        manufacturer: {
+          id: 100,
+          name: "Wilson",
+          origin: "Chicago, Illinois",
+        },
+        name: "Baseball Glove",
+        outOfStock: false,
+        price: 99.99,
+        relatedItems: ["Baseball Bat", "Baseball", "Baseball Helmet"],
+      };
+
+      const result = toSerializableJsonObj(input);
+
+      expect(result).toEqual(input);
+    });
+
+    test("converts BigInts to Numbers", async () => {
+      const input = {
+        artist: {
+          id: BigInt(200),
+          name: "Michael Jackson",
+        },
+        id: BigInt(100),
+        releaseDate: "January 2, 1983",
+        streams: BigInt(1000000000),
+        title: "Billie Jean",
+      };
+      const expected = {
+        artist: {
+          id: 200,
+          name: "Michael Jackson",
+        },
+        id: 100,
+        releaseDate: "January 2, 1983",
+        streams: 1000000000,
+        title: "Billie Jean",
+      };
+
+      const result = toSerializableJsonObj(input);
+
+      expect(result).toEqual(expected);
+    });
+  });
+});
