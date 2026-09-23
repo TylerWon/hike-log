a8e0e0a019b2bcaecde34b0f444ff3d4fe997ed1
Tyler Won <won.tyler1@gmail.com>
2026-09-22 23:01:00 -0700

    Move create photo endpoint to hike handler file

--- backend/handler/hike.go
+++ backend/handler/hike.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/TylerWon/hike-log/backend/models"
@@ -98,6 +101,75 @@ func (h *Handler) CreatePhotoUploadURL(c *gin.Context) {
 
 	res := CreatePhotoUploadURLResponse{presignedReq.URL, objectKey}
 	c.JSON(http.StatusOK, res)
+}
+
+/*
+Creates a Photo for a Hike.
+
+The photo should already be uploaded to S3. This endpoint is just responsible for creating the Photo model in the DB.
+
+Path parameters: [hikeIDPathParam]
+
+Request body: [createPhotoRequest]
+
+Returns:
+ 1. 201 Created and the [models.Photo] when successful
+ 2. 400 Bad Request and an error message when input is bad
+ 3. 404 Not Found and an error message when the Photo does not exist
+ 4. 500 Internal Server Error and an error message when there is an unexpected error
+*/
+func (h *Handler) CreatePhoto(c *gin.Context) {
+	var params hikeIDPathParam
+	if err := c.ShouldBindUri(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err := h.store.GetHikeByID(params.HikeID)
+	if err != nil {
+		handleRecordDoesNotExistError(c, err, params.HikeID)
+		return
+	}
+
+	var req createPhotoRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Skip validation here since ObjectKey is validated when request body gets binded to [createPhotoRequest]
+	keyParts := strings.Split(req.ObjectKey, "/")
+	hikeId, _ := strconv.ParseUint(keyParts[1], 10, 64)
+	if hikeId != uint64(params.HikeID) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Hike ID in path and ObjectKey do not match"})
+		return
+	}
+
+	exists, err := h.s3Client.DoesObjectExist(c, req.ObjectKey)
+	if err != nil {
+		log.Printf("Unable to verify if photo object (key=%s) exists in S3 bucket: %v", req.ObjectKey, err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": http.StatusText(http.StatusInternalServerError)})
+		return
+	} else if !exists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Photo with provided ObjectKey does not exist in S3 bucket"})
+		return
+	}
+
+	srcURL := h.s3Client.GetObjectURL(req.ObjectKey, os.Getenv("ENV"))
+	photo := models.Photo{
+		SrcUrl:       srcURL,
+		Caption:      req.Caption,
+		DisplayOrder: req.DisplayOrder,
+		HikeID:       params.HikeID,
+	}
+	err = h.store.CreateRecord(&photo)
+	if err != nil {
+		log.Println("Failed to create Photo: ", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": http.StatusText(http.StatusInternalServerError)})
+		return
+	}
+
+	c.JSON(http.StatusCreated, photo)
 }
 
 /*
